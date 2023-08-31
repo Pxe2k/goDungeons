d6a01c6a94879e1a6e2eda737e4a4ff8c0512003
pxe2k <roariffic474@gmail.com>
2023-08-31 06:28:27 +0600

    ost added

--- game/audio.go
+++ game/audio.go
@@ -1,1 +1,29 @@
 package game
+
+import (
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
+	"os"
+)
+
+func PlayMusic() (*audio.Player, error) {
+	audioContext := audio.NewContext(44100)
+
+	f, err := os.Open("resource/audio/macan-asphalt-8.wav")
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	d, err := wav.DecodeWithSampleRate(44100, f)
+	if err != nil {
+		return nil, err
+	}
+
+	audioPlayer, err := audioContext.NewPlayer(d)
+	if err != nil {
+		return nil, err
+	}
+
+	return audioPlayer, nil
+}
--- main.go
+++ main.go
@@ -1,12 +1,37 @@
 package main
 
 import (
+	"fmt"
 	g "github.com/Pxe2k/goDungeons/game"
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
+	"os"
 )
 
 func main() {
 	ebiten.SetWindowSize(g.ScreenSize())
 	ebiten.SetWindowTitle("Hasan Game")
+
+	//TODO move this code to audio.go file
+	audioContext := audio.NewContext(44100)
+
+	f, err := os.Open("resource/audio/macan-asphalt-8.wav")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer f.Close()
+
+	d, err := wav.DecodeWithSampleRate(44100, f)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	audioPlayer, err := audioContext.NewPlayer(d)
+	if err != nil {
+		fmt.Println(err)
+	}
+	audioPlayer.Play()
+
 	ebiten.RunGame(g.NewGame())
 }
